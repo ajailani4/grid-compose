ca2370ef179fc344d11e028f24b53612f85a6256
Ahmad Jailani <ahmadjailani604@gmail.com>
2025-01-20 15:24:47 +0700

    Add publishing script

--- grid_compose/build.gradle.kts
+++ grid_compose/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    `maven-publish`
 }
 
 android {
@@ -50,4 +51,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.ajailani4"
+                artifactId = "grid-compose"
+                version = "1.0.0"
+            }
+        }
+    }
 }
